2356d8526c90be1e816b1a6dce291a0c283488e8
codekidX <ckrackerx15@gmail.com>
2014-11-07 15:19:08 +0530

    cpufreq: revert only addition of hotplug

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -370,9 +370,6 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
 extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lulzactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
-extern struct cpufreq_governor cpufreq_gov_hotplug;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
 #endif
 
 
